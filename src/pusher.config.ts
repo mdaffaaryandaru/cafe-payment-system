821e9319839a4251373fe344b140f0b365f23751
mdaffaaryandaru <daffaaryandaru100815@gmail.com>
2024-08-20 18:57:41 +0700

    Remove pusher.config.ts from tracking

--- src/pusher.config.ts
+++
@@ -1,9 +0,0 @@
-const Pusher = require('pusher');
-
-export const pusher = new Pusher({
-  appId: '1852344',
-  key: 'eeac291f5408aa1cf514',
-  secret: '6832b230f40ad06b09e8',
-  cluster: 'ap1',
-  useTLS: true,
-});
