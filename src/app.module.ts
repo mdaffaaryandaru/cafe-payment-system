4237318b8c2dcc7419a61193de41245d3ff89f8d
mdaffaaryandaru <daffaaryandaru100815@gmail.com>
2024-08-24 17:13:17 +0700

    config on env

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,27 +12,33 @@ import { Order } from './order/order.entity';
 import { DetailOrderan } from './order/detail-orderan.entity';
 // import { WebSocketGatewayService } from './websocket.gateway';
 import { AppGateway } from './app.gateway';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TopingModule } from './toping/toping.module';
 import { Toping } from './toping/entities/toping.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'seruni_database',
-      entities: [Menu, Pegawai, Order, DetailOrderan, Toping],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): TypeOrmModule => ({
+        type: configService.get<'mysql'>('DB_TYPE'), // Pastikan tipe ini sesuai dengan yang didukung oleh TypeORM
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Menu, Pegawai, Order, DetailOrderan, Toping],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     MenuModule,
     PegawaiModule,
     OrderModule,
     TopingModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
